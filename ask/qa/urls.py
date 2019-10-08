ff918f8947cf9817e49a16c0c37a697b121709cf
doutelet <55977726+doutelet@users.noreply.github.com>
2019-10-08 18:40:12 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,1 +1,14 @@
+from ask.qa.views import test
+from django.conf.urls import url
 
+    url(r'^$', )
+   # TODO 
+   # r'^login/', include('qa.urls')
+   # |
+   # v
+    url(r'^login/', )
+    url(r'^signup/', )
+    url(r'^question/[0-9]+/', test)
+    url(r'^ask/', )
+    url(r'^popular/', )
+    url(r'^new/', )
