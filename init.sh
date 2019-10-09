9118ebf2b442c919a3e20eb37a58730f08953e56
doutelet <55977726+doutelet@users.noreply.github.com>
2019-10-09 16:09:32 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -b 0.0.0.0:8080 hello:app
-sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:app
+sudo gunicorn -b 0.0.0.0:8000 ask.ask.wsgi:app
